26c680281a67b516db1092a4686ba67fb028c009
marcy3ait <39223917+marcy3ait@users.noreply.github.com>
2021-05-06 12:28:53 -0300

    update (testar otimizacao)

--- otimizacao/class_AG.py
+++ otimizacao/class_AG.py
@@ -1,27 +1,25 @@
 #%%
 import numpy as np
 import random
+
 # importar interface xfoil
 
 func = lambda a: sum(a)
 
-class Genetic_simple:
+class Genetic_simple(object):
     ''' Functions SGA '''
 
-    def __init__(self, ngen, npop, pmut, pcross, lim_up, lim_down):
+    def __init__(self, ngen, npop, pmut, lim_up, lim_down):
+        
         self.ngen = int(ngen) # numero de geracoes 
         self.npop = int(npop) # tamanho da populacao
         self.pmut = float(pmut) # probalidade de mutacao de cada gene
-        self.pcross = float(pcross) # probabilidade de crossing 
 
         self.lim_up = lim_up # limite inferior
         self.lim_down = lim_down # limite superior
 
         self.nvar = len(self.lim_up) # numero de genes do cromossso
 
-        # gerando a populacao
-        self.pop = np.zeros((self.npop, self.nvar)) #self.population() # vetor(nº de indiviuos da pop., nº de cromossos de cada individuo)
-        
 
     def restricao(self, ind):
 
@@ -43,9 +41,13 @@ class Genetic_simple:
     def population(self):
 
         ''' cria a pop. de individuos '''
+        populacao = list()
 
         for i in range(self.npop):
-            self.pop[i][:] = self._pop_init()
+
+            populacao.append( self._pop_init() )
+
+        return populacao
         
 
     #-------------- funcoes basicas AGs ------------------
@@ -53,15 +55,28 @@ class Genetic_simple:
     def mutacao_float(self, individuo):
 
         ''' operador de mutacao para cod. em ponto flutuante  - mutacao uniforme '''
-        # verificar uma outra implentação -> gaussiana ou outra conviniente
+        # verificar uma outra implentação -> gaussiana ou outra conviniente pob = 0.05
         
-        for i, xl, xu in zip(self.nvar, self.lim_down, self.lim_up):
+        for i, xl, xu in zip(range(self.nvar), self.lim_down, self.lim_up):
             if random.random() < self.pmut:
                 individuo[i] = random.uniform(xl, xu)
         
 
         return self.restricao(individuo)
     
+    def mutacao(self, populacao):
+
+        ''' recebe um populacao para aplicar o operador de mutacao '''
+
+        for i in range(len(populacao)):
+            # cada indiv.
+            
+            ## mutacao -> mod. 1 ind
+            populacao[i] = self.mutacao_float(populacao[i])
+
+        return populacao
+  
+
     def crossover(self, individuoA, individuoB):
 
         ''' operador de crossover - um ponto de troca (mais simples) '''
@@ -71,19 +86,51 @@ class Genetic_simple:
 
         return self.restricao(individuoA), self.restricao(individuoB) 
 
+    def filhos(self, pop):
+        
+        ''' gera a pop. de filhos '''
+        populacao = pop.copy()
+        pop_aux = list()
 
-    def selecao(self, numInd):
+        while( len(populacao) > 0):
+            
+            pai = random.choice(populacao)
+            populacao.remove(pai)
+            mae = random.choice(populacao)
+            populacao.remove(mae)
+            
+            filho1, filho2 = self.crossover(pai,mae)
+
+            pop_aux.append( filho1 )
+            pop_aux.append( filho2 )
+
+        return pop_aux
+
+        
+
+    def mistura(self, populacaoA, populacaoB):
+        
+
+        pop_total = list()
+
+        pop_total.extend(populacaoA)
+        pop_total.extend(populacaoB)
+        
+        return pop_total
+
+
+    def selecao(self, populacao, numInd):
         
         ''' operador selecao - seleciona os melhores individuos '''
-        melhores = []
-        aux = []
-        melhores = ( [self.fitness(func, ind), ind] for ind in self.pop)
+        melhores = list()
+        aux = list()
+        aux = ( [self.fitness(func, ind), ind] for ind in populacao )
         #print(sorted(melhores, reverse=True)[:numInd])
         #_, aux = sorted(melhores, reverse=True)[:numInd]
-        ord = sorted(melhores, reverse=True)[:numInd]
-        aux = [a for _, a in ord]
+        ord = sorted(aux, reverse=True)[:numInd]
+        melhores = [a for _, a in ord]
 
-        return np.array(aux)
+        return melhores
 
     def fitness(self, func, individuo):
 
@@ -94,17 +141,6 @@ class Genetic_simple:
 
         return getValue
 
-    #-------------- visualizar ------------------------
-    
-    def display(self):
-        pass
-
-class runSga(Genetic_simple):
-    ''' Run SGA - simple genetic algorith '''
-
-    def __init__(self):
-
-        super().__init__()
 
     def run(self):
 
@@ -112,27 +148,67 @@ class runSga(Genetic_simple):
             # cada pop.
 
             if j == 0:
-               self.population() # inicializa a pop.
+                # inicializando o vetor populacao self.pop[n_geracao][n_individuos]
+               
+                pop = self.population() # inicializa a pop.
 
-            for i in range(0, self.npop):
-                # cada indiv.
+            #print(len(pop))
+            # passando a pop toda para o operador de mutacao
+            
+            #print('\n')
+            #print(' antes da mutacao' ,pop)
+            pop = self.mutacao(pop)
+            #print('\n')
+            #print(' depois da mutacao' ,pop)
+           
 
-                ## mutacao -> mod. 1 ind
-                self.pop[j][i] = self.mutacao_float(self.pop[j][i])
+            #print(len(pop))
+            ## crossover -> mod. 2 ind. (filhos)]
+            pop1 = pop.copy()
+            filhos = self.filhos(pop1)
+
+            #print('len filhos ',len(filhos))
+            ## mistura
+            pop_aux = self.mistura(pop, filhos) 
+
+            #print(len(pop),pop)
+            # melhor individuo e fitness
+            fitness, melhor = self.melhorInd(pop)
+            print( f' Geracao: {j} ')
+            print( f' Melhor fitness: {fitness} , {melhor}')
+            print( f' Tamanho da pop.: {len(pop)}')
+            #print( f' Melhor individuo: {melhor} ')
 
             ## selecao
-
-            ## crossover -> mod. 2 ind.
+            pop = self.selecao(pop_aux, len(pop))
             
 
+    #-------------- visualizar ------------------------
+    
+    def melhorInd(self, populacao):
+        
+        aux = []
+        aux = ( [self.fitness(func, ind), ind] for ind in populacao )
+        #print(sorted(melhores, reverse=True)[:numInd])
+        #_, aux = sorted(melhores, reverse=True)[:numInd]
+        #print(sorted(aux, reverse=True)[0])
+        fitness, melhor = sorted(aux, reverse=True)[0]
+    
 
-
+        return fitness, melhor
+        
 
 
 if __name__ == "__main__":
-    inf = [0.3, 0.3, 0.3, 0.3, 0.3] 
-    sup = [0.7, 0.7, 0.7, 0.7, 0.7] 
-    sga = Genetic_simple(ngen = 10, npop = 20, pmut = 0.05, pcross = 0.002, lim_down = inf, lim_up= sup )
+    inf = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3] 
+    sup = [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7] 
+    #algoritmo_genetico = Genetic_simple()
+    # npop deve ser par
+
+    sga = Genetic_simple(ngen = 500, npop = 200, pmut = 0.05, lim_up= sup, lim_down = inf )
+    sga.run()
+
+   
 
 
 
